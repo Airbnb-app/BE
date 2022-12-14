45f2999ca26732edab7891b7e8d43eb6f7ed15b0
aziz-wahyudin <midnightmist37@gmail.com>
2022-12-15 06:15:47 +0700

    update payment association

--- features/payment/entity.go
+++ features/payment/entity.go
@@ -1,35 +1,21 @@
 package payment
 
-type FirstCore struct {
-	ReservationRequest ReservationRequest
-	HomestayResponse   HomestayResponse
-	PaymentCore        PaymentCore
+import "time"
+
+type Payment struct {
+	ID            uint
+	CreditCard    string
+	Name          string
+	CardNumber    string
+	Cvv           string
+	Month         string
+	Year          string
+	ReservationID uint
 }
 
-type PaymentCore struct {
-	CreditCard string
-	Name       string
-	CardNumber string
-	Cvv        string
-	Month      string
-	Year       string
-}
-
-type ReservationRequest struct {
-	HomestayID int
-	StartDate  string
-	EndDate    string
-}
-
-type HomestayResponse struct {
-	Duration   int
-	TotalPrice int
-}
-
-type ServiceInterface interface {
-	CreatePayment(data FirstCore) error
-}
-
-type RepositoryInterface interface {
-	CreatePayment(data FirstCore) error
+type Reservation struct {
+	ID        uint
+	StartDate time.Time
+	EndDate   time.Time
+	Payment   Payment
 }
