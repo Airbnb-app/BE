2421029076bbbb0d2f04b147e5deb7ecf01c1f27
aziz-wahyudin <midnightmist37@gmail.com>
2022-12-15 07:00:54 +0700

    update paymen association

--- features/payment/repository/model.go
+++ features/payment/repository/model.go
@@ -7,19 +7,21 @@ import (
 )
 
 type Payment struct {
-	gorm.Model
 	CreditCard    string
 	Name          string
 	CardNumber    string
 	Cvv           string
 	Month         string
 	Year          string
-	ReservationID uint
+	ReservationID uint `gorm:"primaryKey"`
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	DeletedAt     gorm.DeletedAt `gorm:"index"`
 }
 
 type Reservation struct {
 	gorm.Model
 	StartDate time.Time
 	EndDate   time.Time
-	Payment   Payment
+	Payment   Payment `gorm:"foreignKey:ReservationID"`
 }
